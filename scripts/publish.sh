17ce93f1349f656db384d2d07999482b6b9d45f2
Romulo Franca <romulo2franca@gmail.com>
2019-12-23 16:55:44 -0300

    change config

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,8 @@
 set -e
-
 docker tag ${PROJECT_NAME}:latest ${REPO_URL}/${PROJECT_NAME}:latest
 docker tag ${PROJECT_NAME}:latest ${REPO_URL}/${PROJECT_NAME}:${GIT_COMMIT}
 
-docker push ${REPO_URL}/${PROJECT_NAME}:latest
-docker push ${REPO_URL}/${PROJECT_NAME}:${GIT_COMMIT}
+docker.withRegistry('', 'dockerhub'){
+  docker push ${REPO_URL}/${PROJECT_NAME}:latest
+  docker push ${REPO_URL}/${PROJECT_NAME}:${GIT_COMMIT}
+}
